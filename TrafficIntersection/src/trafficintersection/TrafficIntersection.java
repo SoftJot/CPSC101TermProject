6d16daf15e1e9651777877c095666d1acfaed1b8
jcbotha <jcbotha@me.com>
2014-02-10 11:37:00 -0800

    Included Team names in Main File

--- TrafficIntersection/src/trafficintersection/TrafficIntersection.java
+++ TrafficIntersection/src/trafficintersection/TrafficIntersection.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * CPSC Team Project - Traffic Intersection
+ * Team: Darren Hendrickson, Clinton Edwards, Chris Botha
+ * 
  */
 
 package trafficintersection;
